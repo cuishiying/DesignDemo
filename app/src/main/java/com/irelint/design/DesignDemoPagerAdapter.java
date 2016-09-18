1fdbef444a3d81dfdc77e2546b15941123accaa2
cuishiying <cuishiying163@163.com>
2016-09-18 14:28:26 +0800

    first commit

---
+++ app/src/main/java/com/irelint/design/DesignDemoPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.irelint.design;
+
+/**
+ * 作者：当我遇上你 on 2016-9-18 14:00
+ * 邮箱：cuishiying163@163.com
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class DesignDemoPagerAdapter extends FragmentStatePagerAdapter {
+
+    public DesignDemoPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return DesignDemoFragment.newInstance(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Tab " + position;
+    }
+}
---
+++ app/src/main/java/com/irelint/design/MainActivity.java
@@ -0,0 +1,22 @@
+package com.irelint.design;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        DesignDemoPagerAdapter adapter = new DesignDemoPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
